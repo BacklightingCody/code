701904fb37374242863f320f71736f89ec95aba4
backlighting <pitilessmiracle@outlook.com>
2024-09-21 17:12:22 +0800

    code：动态规划，最大正方形

---
+++ code/middle/221.js
@@ -0,0 +1,24 @@
+const matrix = [["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]]
+
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalSquare = function (matrix) {
+  const m = matrix.length
+  const n = matrix[0].length
+  const dp = Array(m).fill(0).map(() => Array(n).fill(0))
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === 0 || j === 0) {
+        dp[i][j] = matrix[i][j] === '1' ? 1 : 0
+      }
+      else if (matrix[i][j] === '1') {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+      }
+    }
+  }
+  return Math.max(...dp.flat(Infinity)) ** 2
+};
+
+console.log(maximalSquare(matrix))
